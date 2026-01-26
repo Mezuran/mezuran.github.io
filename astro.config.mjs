88e3aa8c0a26bb07338ef92a0a60486db103a9b5
Mitho Mizuno <mithomizuno@gmail.com>
2026-01-26 15:02:43 +0700

    Modifying base route to /

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://Mezuran.github.io',
-    base: '/Mezuran'
+    site: 'https://mezuran.github.io',
+    base: '/'
 });
